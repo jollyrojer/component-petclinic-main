a0dbe3eb9eae7ea26901dcfe92b565f7740654c7
Artem Ivantsov <aivantsov@qubell.com>
2015-02-12 16:47:10 +0200

    Petclinic on compute.Instance

--- test/test.py
+++ test/test.py
@@ -18,11 +18,11 @@ def check_site(instance):
     # Check we have 2 hosts up
     @eventually(AssertionError, KeyError)
     def eventually_assert():
-        assert len(instance.returnValues['endpoints.entry'])
+        assert len(instance.returnValues['petclinic.entry-url'])
     eventually_assert()
 
     # Check site still alive
-    url = instance.returnValues['endpoints.entry']
+    url = instance.returnValues['petclinic.entry-url']
     resp = requests.get(url)
     assert resp.status_code == 200
     assert 'PetClinic :: a Spring Framework demonstration' in resp.text
@@ -76,11 +76,11 @@ def check_site(instance):
 })
 class PetClinicComponentTestCase(BaseComponentTestCase):
     name = "starter-java-web"
-    meta = "https://raw.githubusercontent.com/qubell-bazaar/starter-java-web/master/meta.yml"
+    meta = os.path.realpath(os.path.join(os.path.dirname(__file__), '../meta.yml'))  
     db_name = "petclinic"
     apps = [{
         "name": name,
-        "settings": {"destroyInterval": 7200000},
+        "settings": {"destroyInterval": 14400000},
         "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
    }]
     @classmethod
@@ -88,17 +88,17 @@ class PetClinicComponentTestCase(BaseComponentTestCase):
         return 120
  
     @instance(byApplication=name)
-    @values({"lb-host": "host"})
-    def test_host(self, instance, host):
-        resp = requests.get("http://" + host, verify=False)
+    def test_host(self, instance):
+        host = instance.returnValues['petclinic.entry-url']
+        resp = requests.get(host, verify=False)
 
         assert resp.status_code == 200
 
     @instance(byApplication=name)
-    @values({"db-port": "port", "db-host": "host"})
-    def test_db_port(self, instance, host, port):
+    def test_db_port(self, instance):
         import socket
-
+        host = instance.returnValues['petclinic.dbms']['db-host']
+        port = instance.returnValues['petclinic.dbms']['db-port']
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((host, port))
 
@@ -110,9 +110,9 @@ class PetClinicComponentTestCase(BaseComponentTestCase):
 
     @instance(byApplication=name)
     def test_scaling(self, instance):
-        assert len(instance.returnValues['endpoints.app']) == 1
-        params = {'input.app-quantity': '2',
-                 'input.app-branch': instance.parameters['input.app-branch']}
+        assert len(instance.returnValues['petclinic.app-hosts']) == 1
+        params = {'configuration.clusterSize': '2',
+                 'configuration.scm-branch': instance.parameters['configuration.scm-branch']}
         instance.reconfigure(parameters=params)
         assert instance.ready(timeout=30)
 
@@ -120,17 +120,18 @@ class PetClinicComponentTestCase(BaseComponentTestCase):
         # Check we have 2 hosts up
         @eventually(AssertionError, KeyError)
         def eventually_assert():
-            assert len(instance.returnValues['endpoints.app']) == 2
+            assert len(instance.returnValues['petclinic.app-hosts']) == 2
         eventually_assert()
 
     @instance(byApplication=name)
-    @values({"lb-host": "host"})
-    def test_change_branch(self, instance, host):
-        params = {'input.app-branch': 'red'}
+    def test_change_branch(self, instance):
+        params = {'configuration.scm-branch': 'red'}
         instance.reconfigure(parameters=params)
-        assert instance.ready(timeout=20)
-
+        assert instance.ready(timeout=30)
+        host = instance.returnValues['petclinic.entry-url']
         check_site(instance)
-        resp = requests.get("http://" + host, verify=False)
-
-        assert 'Updated PetClinic :: a Spring Framework demonstration' in resp.text
+        @eventually(AssertionError, KeyError)
+        def eventually_assert():
+            resp = requests.get(host, verify=False)
+            assert 'Updated PetClinic :: a Spring Framework demonstration' in resp.text
+        eventually_assert()
